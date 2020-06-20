0b7dc395b8b5dce04334658464adc9698f3d1a02
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-06-20 13:58:20 -0700

    adding the client folder

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
