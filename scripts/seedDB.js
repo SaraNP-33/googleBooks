89bbf569e77bd782350d14910802abb222bd9daa
lindsey tummond <lindseytummond@gmail.com>
2020-06-26 21:05:28 -0700

    added seeds

---
+++ scripts/seedDB.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch",
+{
+  useCreateIndex: true,
+  useNewUrlParser: true
+}); 
+
+const bookSeed = [
+  {
+    authors: ["Suzanne Collins"],
+    description: "Set in a dark vision of the near future, a terrifying reality TV show is taking place. Twelve boys and twelve girls are forced to appear in a live event called The Hunger Games. There is only one rule: kill or be killed. When sixteen-year-old Katniss Everdeen steps forward to take her younger sister's place in the games, she sees it as a death sentence. But Katniss has been close to death before. For her, survival is second nature.",
+    image: "http://books.google.com/books/content?id=sazytgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    link: "http://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
+    title: "The Hunger Games",
+    date: new Date(Date.now())
+}
+];
+
+db.Book
+  .remove({})
+  .then(() => db.Book.collection.insertMany(bookSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
+const routes = require("./routes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,16 +13,18 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
-
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch";
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
+// Add routes, both API and view
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
