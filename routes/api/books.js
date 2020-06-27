cb332566ecfaa808b0c9a492c095d47b5f4e0efb
lindsey tummond <lindseytummond@gmail.com>
2020-06-26 21:07:22 -0700

    updating routes

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
-​
+
 // Matches with "/api/books"
-router.route("/api/books")
+router.route("/")
   .get(booksController.findAll)
   .post(booksController.create);
-​
+
 // Matches with "/api/books/:id"
 router
-  .route("api/books/:id")
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
   .delete(booksController.remove);
-​
+
 module.exports = router;
